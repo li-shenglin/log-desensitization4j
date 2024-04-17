33c549888acf4ffd4fef50f9d6035b36a64878db
shenglin.li <764935704@qq.com>
2024-04-17 19:50:02 +0800

    version0.3 reload

--- src/main/java/io/github/li_shenglin/desensitization/springcloud/ConfigInitRunner.java
+++ src/main/java/io/github/li_shenglin/desensitization/springcloud/ConfigInitRunner.java
@@ -28,5 +28,6 @@ public class ConfigInitRunner implements CommandLineRunner  {
                 throw new RuntimeException(e);
             }
         });
+        DesensitizationFactory.reload();
     }
 }
